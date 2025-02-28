501347ca0804188721199c115deb6ec384a6d185
Joseph321123 <aguilarrodriguez1.15@gmail.com>
2025-02-28 10:14:55 -0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/burguer_app_ui_2b_aguilar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.burguer_app_ui_2b_aguilar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/onboard_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OnBoardPage extends StatefulWidget {
+  const OnBoardPage({super.key});
+
+  @override
+  State<OnBoardPage> createState() => _OnBoardPageState();
+}
+
+class _OnBoardPageState extends State<OnBoardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Image.asset('assets/food pattern.png'),
+      ),
+    );
+  }
+}
